66e3ed65c3f696126f6f605926562092cb7c810d
Invisible Arts <invisiblearts@outlook.com>
2017-03-26 22:48:35 +0800

    optimize get_array()

--- ivsfunc.py
+++ ivsfunc.py
@@ -1,6 +1,6 @@
 import vapoursynth as vs
 import numpy as np
-
+import ctypes
 
 # CHW if not HWC
 def get_array(src, frame_num, HWC=True):
@@ -13,9 +13,28 @@ def get_array(src, frame_num, HWC=True):
     num_planes = frame.format.num_planes
 
     out_list = []
+    
+    st = frame.format.sample_type
+    bps = frame.format.bytes_per_sample
+
+    if st == vs.INTEGER:
+        if bps == 1:
+            dtype = ctypes.c_uint8
+        elif bps == 2:
+            dtype = ctypes.c_uint16
+        else:
+            raise ValueError('Wrong sample type!')
+    elif st == vs.FLOAT:
+        if bps == 2:
+            raise ValueError('Half sample is not natively supported! Please resample it.')
+        elif bps == 4:
+            dtype = ctypes.c_float
+        else:
+            raise ValueError('Wrong sample type!')
+
 
     for i in range(num_planes):
-        arr = np.array(frame.get_read_array(i))
+        arr = np.ctypeslib.as_array((dtype * frame.width * frame.height).from_address(frame.get_read_ptr(i).value))
         out_list.append(arr)
 
     # check to prevent subsampling issues
@@ -35,6 +54,9 @@ def preview_frame(src, frame_num, **kwargs):
     from mvsfunc import Preview
     from scipy.misc import imsave
     from io import BytesIO
+
+    assert(isinstance(src, vs.VideoNode))
+    assert(isinstance(frame_num, int))
 
     if src.format.color_family is not vs.GRAY:
         src = Preview(src, **kwargs)
